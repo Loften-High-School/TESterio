de65f47321bb7605e2f3f88ae1ac4434b8443f72
QAlto-Sax <meatbrick2005@gmail.com>
2026-03-25 10:00:33 -0400

    Scripts-and-gameover

--- Assets/Scripts/Platformmovement.cs
+++ Assets/Scripts/Platformmovement.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
 
-public class Platformmovement : MonoBehaviour
+public class PlatformMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+   public float speed = 2f;
+   public Transform groundCheck;
+   public LayerMask groundLayer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private Rigidbody2D rb;
+   private bool movingRight = true;
+   
+   void Start()
+   {
+      rb = GetComponent<Rigidbody2D>();
+   }
+
+
+   private void FixedUpdate()
+   {
+      rb.linearVelocity = new Vector2((movingRight ? 1 : -1) * speed, rb.linearVelocity.y);
+
+      RaycastHit2D groundInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, 1f, groundLayer);
+
+      if(groundInfo.collider == false)
+      {
+         Flip();
+      }
+   }
+
+   private void OnCollisionEnter2D(Collision2D collision)
+   {
+      if (collision.collider.CompareTag("Ground"))
+      {
+         Flip();
+      }
+   }
+
+   void Flip()
+   {
+      movingRight = !movingRight;
+      Vector3 scaler = transform.localScale;
+      scaler.x *= -1;
+      transform.localScale = scaler;
+   }
 }
