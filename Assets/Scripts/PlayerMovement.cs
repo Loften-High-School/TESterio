82ba8e541b4b44b77639ebca7e68b02b41b1c77b
QAlto-Sax <meatbrick2005@gmail.com>
2026-03-31 10:00:04 -0400

    Push

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,127 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerStuff : MonoBehaviour
+{
+    private float speed = 8f;
+   private bool isWallSliding;
+    private bool isFacingRight = true;
+    private float horizontal;
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+
+[SerializeField] private Rigidbody2D rb;
+[SerializeField] private Transform groundCheck;
+[SerializeField] private LayerMask groundLayer;
+[SerializeField] private TrailRenderer tr;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+
+    private bool canJump;
+    public Rigidbody2D rb2D;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        canJump = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveValue = InputSystem.actions.FindAction("Move").ReadValue<Vector2>();
+
+        transform.Translate (moveValue.x * Time.deltaTime * 5, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.W) && canJump == true)
+        {
+            rb2D.AddForce(Vector2.up * 7f, ForceMode2D.Impulse);
+            canJump = false;
+        }
+
+        WallJump();
+
+        if (!isWallJumping)
+        {
+            Flip();
+        }
+    }
+
+
+     private void FixedUpdate()
+    {
+        if (!isWallJumping)
+        {
+            rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D otherObject)
+    {
+            canJump = true;
+    }
+
+
+        private void WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.linearVelocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDirection)
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
