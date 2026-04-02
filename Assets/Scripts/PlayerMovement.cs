06431984eb9c8ab42e822f1ac1d8f47b47c22307
Durpasaurus <elisha.farmer0806@gmail.com>
2026-04-02 10:00:11 -0400

    player movement update 1.0

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class Playermovement : MonoBehaviour
 {
 public Rigidbody2D rb;
+public bool left = false;
+public bool right = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,7 +20,37 @@ public Rigidbody2D rb;
 
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            rb.AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * 17.5f, ForceMode2D.Impulse);
+        }
+         if(Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(Vector2.up * 17.5f, ForceMode2D.Impulse);
+        }
+         if(Input.GetKeyDown(KeyCode.S))
+        {
+            rb.AddForce(Vector2.down * 17.5f, ForceMode2D.Impulse);
+        }
+         if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            rb.AddForce(Vector2.down * 17.5f, ForceMode2D.Impulse);
+        }
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            left = true;
+            right = false;
+        }
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            left = false;
+            right = true;
+        }
+         if(Input.GetKeyDown(KeyCode.Q) && left == true && right == false)
+        {
+            rb.AddForce(Vector2.left * 17.5f);
+        }
+        if(Input.GetKeyDown(KeyCode.Q) && right == true && left == false)
+        {
+            rb.AddForce(Vector2.right * 17.5f);
         }
     }
 
