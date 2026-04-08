146dd8525353a882ffe65a8a2e37d2268a137ccc
Durpasaurus <elisha.farmer0806@gmail.com>
2026-04-08 10:01:19 -0400

    very small player movement update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class Playermovement : MonoBehaviour
 public Rigidbody2D rb;
 public bool left = false;
 public bool right = false;
+public int jumps = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
