f2b129b914ed4ffd182a22fd369c1506e3331e18
Due3diligence <235400266+Due3diligence@users.noreply.github.com>
2026-04-29 09:12:27 -0400

    Game Summary
    This is Immanuel's branch and I have made level 3 of the game but I ran into some scripts problem which I am currently working on.

--- Assets/Scripts/spike script.cs
+++ Assets/Scripts/spike script.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Platformmovement : MonoBehaviour
+public class spikescript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,5 +12,12 @@ public class Platformmovement : MonoBehaviour
     void Update()
     {
         
+    }
+    void onTriggerEnter2d()
+    {
+        if(otherObject.gameObject.CompareTag("spikes lvl 1"))
+        {
+            transform.position = new Vector3(-14.41f, 2.82f, 0.0f);
+        }
     }
 }
